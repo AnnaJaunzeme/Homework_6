635af95bbae8857c7310aa76722ed19e9173c4d7
Anna Jaunzeme <anna.jaunzeme@inbox.lv>
2021-10-31 21:51:07 +0200

    Homework6

---
+++ Homework_6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Homework_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array[] Array1 = { new Array(12345), new Array(24), new Array(99), new Array(1709), new Array(1)};
+            Array[] Array2 = new Array[5];
+
+            Array1.CopyTo(Array2, 0);
+
+            Console.WriteLine("Array1: ");
+            foreach (Array elements in Array1)
+            {
+                Console.WriteLine(elements.element);
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Array2: ");
+            foreach (Array elements in Array2)
+            {
+                Console.WriteLine(elements.element);
+            }
+            Console.ReadLine();
+        }
+        public class Array
+        {
+            public int element;
+            public Array (int element)
+            {
+                this.element = element;
+            }
+        }
+    }
+}
